bdd7a8b58a2e4835f18bd41a1cfa03400afda25b
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-09-04 16:48:29 +0800

    configure .env file for database hosting

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->int('isPendingResident')->nullable();
+            $table->integer('isPendingResident')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
